e46e60cb13b76695260579ac3cd858da0eb65e90
Hannu Lyytinen <hannu.lyytinen@unikie.com>
2021-11-24 19:09:06 +0200

    HYPR-301 Add our seL4-specific layer
    Signed-off-by: Hannu Lyytinen <hannu.lyytinen@unikie.com>

--- yocto/setup.sh
+++ yocto/setup.sh
@@ -3,4 +3,5 @@ export MACHINE=raspberrypi4-64
 
 . poky/oe-init-build-env
 
+grep meta-sel4 conf/bblayers.conf 2>/dev/null 1>&2 || echo 'BBLAYERS += "/workspace/vm-images/meta-sel4"' >> conf/bblayers.conf
 grep meta-raspberrypi conf/bblayers.conf 2>/dev/null 1>&2 || echo 'BBLAYERS += "/workspace/vm-images/meta-raspberrypi"' >> conf/bblayers.conf
