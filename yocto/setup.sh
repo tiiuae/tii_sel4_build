66d9f735a3bb7ae61ee7db0d6858586f5063da01
Hannu Lyytinen <hannu.lyytinen@unikie.com>
2021-11-24 19:08:19 +0200

    HYPR-300 Use RPi4 as build baseline
    Signed-off-by: Hannu Lyytinen <hannu.lyytinen@unikie.com>

--- yocto/setup.sh
+++ yocto/setup.sh
@@ -1,3 +1,6 @@
-export MACHINE=qemuarm64
+# qemuarm64 is a more generic target
+export MACHINE=raspberrypi4-64
 
 . poky/oe-init-build-env
+
+grep meta-raspberrypi conf/bblayers.conf 2>/dev/null 1>&2 || echo 'BBLAYERS += "/workspace/vm-images/meta-raspberrypi"' >> conf/bblayers.conf
