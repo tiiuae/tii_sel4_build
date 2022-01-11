d46518bada0ecb9b3020615a23a558adaa790816
Markku Ahvenjärvi <markku.ahvenjarvi@unikie.com>
2022-01-11 15:14:45 +0200

    Fix hardcoded layer and build paths
    Infer layer paths from setup script location. Also add support to custom
    build directories.

--- yocto/setup.sh
+++ yocto/setup.sh
@@ -1,7 +1,18 @@
+# assume we're in the same dir as layers
+if [ -n "$BASH_SOURCE" ]; then
+  SCRIPT=$BASH_SOURCE
+else
+  SCRIPT=$0
+fi
+LAYERS_ROOT="$(realpath -e "$(dirname "$SCRIPT")")"
+
 # qemuarm64 is a more generic target
 export MACHINE=raspberrypi4-64
 
-. poky/oe-init-build-env
+. "${LAYERS_ROOT}/poky/oe-init-build-env" "$@"
 
-grep meta-sel4 conf/bblayers.conf 2>/dev/null 1>&2 || echo 'BBLAYERS += "/workspace/vm-images/meta-sel4"' >> conf/bblayers.conf
-grep meta-raspberrypi conf/bblayers.conf 2>/dev/null 1>&2 || echo 'BBLAYERS += "/workspace/vm-images/meta-raspberrypi"' >> conf/bblayers.conf
+grep meta-sel4 conf/bblayers.conf 2>/dev/null 1>&2 || \
+  printf 'BBLAYERS += "%s/meta-sel4"\n' "$LAYERS_ROOT" >> conf/bblayers.conf
+
+grep meta-raspberrypi conf/bblayers.conf 2>/dev/null 1>&2 || \
+  printf 'BBLAYERS += "%s/meta-raspberrypi"\n' "$LAYERS_ROOT" >> conf/bblayers.conf
