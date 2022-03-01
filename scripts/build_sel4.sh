f088e5ec8a7ae9df7b68d59d337c667404a4070b
Markku Ahvenjärvi <markkux@ssrc.tii.ae>
2022-03-01 10:54:18 +0200

    Use relative symbolic links
    Get rid of hard coded symbolic link pointing to '/workspace'. Use -r
    option to create relateive symbolic links.
    
    Signed-off-by: Markku Ahvenjärvi <markkux@ssrc.tii.ae>

--- scripts/build_sel4.sh
+++ scripts/build_sel4.sh
@@ -23,8 +23,10 @@ SRCDIR=$1
 
 rm -rf ${BUILDDIR}
 mkdir -p ${BUILDDIR}
-ln -s ../tools/seL4/cmake-tool/init-build.sh ${BUILDDIR}
-ln -s /workspace/${SRCDIR}/easy-settings.cmake ${BUILDDIR}
+
+ln -rs tools/seL4/cmake-tool/init-build.sh "${BUILDDIR}"
+ln -rs "${SRCDIR}/easy-settings.cmake" "${BUILDDIR}"
+
 cd ${BUILDDIR}
 ./init-build.sh -B . -DAARCH64=1 -DPLATFORM=${PLATFORM} -DCROSS_COMPILER_PREFIX=${CROSS_COMPILE} $@
 ninja
