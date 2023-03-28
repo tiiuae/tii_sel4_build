e30fc44c8b72c2c984f1911b4bcfad2619b68769
Markku Ahvenjärvi <markkux@ssrc.tii.ae>
2023-03-28 10:46:57 +0300

    Remove outdated print
    jira: HYPR-365, HYPR-414, HYPR-415
    
    Signed-off-by: Markku Ahvenjärvi <markkux@ssrc.tii.ae>

--- scripts/build_yocto.sh
+++ scripts/build_yocto.sh
@@ -8,8 +8,3 @@ cd vm-images
 . setup.sh
 bitbake vm-image-driver
 bitbake vm-image-boot
-
-DEPLOYDIR=/workspace/projects/camkes-vm-images/${PLATFORM}
-
-echo "Here are your images in ${DEPLOYDIR}: "
-ls -l ${DEPLOYDIR}
