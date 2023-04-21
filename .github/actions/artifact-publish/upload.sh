bacd95596b30e5f7764533e46c19cf5b7dca52ca
Markku Ahvenjärvi <markkux@ssrc.tii.ae>
2023-04-21 08:46:16 +0300

    ci: refactor artifact-publish action
    Add input validation, remove use of sudo and put script to a file.
    
    Signed-off-by: Markku Ahvenjärvi <markkux@ssrc.tii.ae>

---
+++ .github/actions/artifact-publish/upload.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+# Copyright 2023, Technology Innovation Institute
+
+err_print() {
+  printf "%s" "$*" >&2
+}
+
+err_exit() {
+  local rc=$1
+  shift
+  err_print "$@"
+  exit "$rc"
+}
+
+echo "::group::Input validation"
+
+[ ! "$RT_URL" ] && err_exit 1 "RT_URL undefined"
+[ ! "$RT_USER" ] && err_exit 1 "RT_USER undefined"
+[ ! "$RT_API_KEY" ] && err_exit 1 "RT_API_KEY undefined"
+
+for input in $INPUT_PATHS; do
+  SOURCE_DIR=${input%%:*}
+  DEST_DIR=${input#*:}
+  [ ! "$SOURCE_DIR" ] && err_exit 1 "SOURCE_DIR undefined"
+  [ ! "$DEST_DIR" ] && err_exit 1 "SOURCE_DIR undefined"
+done
+
+echo "::endgroup::"
+echo "::group::Artifact upload"
+
+jf c add --url "$RT_URL" --user "$RT_USER" --password "$RT_API_KEY"
+jf rt ping
+
+for input in $INPUT_PATHS; do
+  SOURCE_DIR=${input%%:*}
+  DEST_DIR=${input#*:}
+
+  UPLOAD_DIR=$SOURCE_DIR
+  if [ "$BUILD_NUM" ]; then
+    UPLOAD_DIR=$SOURCE_DIR-b$BUILD_NUM
+    mv "$SOURCE_DIR" "$UPLOAD_DIR"
+  fi
+  jf rt u "$UPLOAD_DIR" "$DEST_DIR" --flat=true
+done
+
+echo "::endgroup::"
