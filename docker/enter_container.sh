f55a5a514b6e21d91d5e1ce5a5a0c3a92d4959b4
Markku Ahvenjärvi <markkux@ssrc.tii.ae>
2023-05-17 09:51:23 +0300

    Fix shellcheck lint warnings
    Signed-off-by: Markku Ahvenjärvi <markkux@ssrc.tii.ae>

--- docker/enter_container.sh
+++ docker/enter_container.sh
@@ -1,20 +1,20 @@
 #! /bin/sh
 
 DIR=$1
-if test "x${DIR}" = "x"; then
-  DIR=`pwd`
+if [ -z "${DIR}" ]; then
+  DIR=$(pwd)
 else
   shift
 fi
 
-CMD=$@
-if test "x${CMD}" = "x"; then
+CMD=$*
+if [ -z "${CMD}" ]; then
   CMD="/bin/bash"
 fi
 
 # Support for non-terminal runs
 INTERACTIVE=
-if test -t 0; then
+if [ -t 0 ]; then
   INTERACTIVE="-it"
 fi
 
@@ -28,16 +28,17 @@ else
 fi
 
 CONTAINER_ENV_FLAGS=
-if test -n "${DOCKER_EXPORT}"; then
+if [ -n "${DOCKER_EXPORT}" ]; then
   CONTAINER_ENV_FLAGS=$(echo "${DOCKER_EXPORT}" | xargs -d ' ' -Ivar -- echo --env var)
 fi
 
+# shellcheck disable=SC2086
 exec ${CONTAINER_ENGINE} run --rm ${INTERACTIVE} \
   ${CONTAINER_ENV_FLAGS} \
-  -v ${DIR}:/workspace:z \
+  -v "${DIR}:/workspace:z" \
   ${YOCTO_SOURCE_MIRROR_DIR:+--env YOCTO_SOURCE_MIRROR_DIR=/workspace/downloads} \
   ${YOCTO_SOURCE_MIRROR_DIR:+-v "${YOCTO_SOURCE_MIRROR_DIR}":/workspace/downloads:z} \
-  -v ${HOME}/.ssh:/home/build/.ssh:z \
-  -v ${HOME}/.gitconfig:/home/build/.gitconfig:z \
+  -v "${HOME}/.ssh:/home/build/.ssh:z" \
+  -v "${HOME}/.gitconfig:/home/build/.gitconfig:z" \
   ${CONTAINER_ENGINE_OPTS} \
-  ${CONTAINER_REGISTRY_PREFIX}tiiuae/build:latest ${CMD}
+  "${CONTAINER_REGISTRY_PREFIX}tiiuae/build:latest" ${CMD}
